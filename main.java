4c65c9e378d558edf08d66f8ac9dd455c7794551
Jonathan MARVIE <jonathan.marvie@gmail.com>
2016-09-01 12:11:50 +0200

    TU Fais Chier

--- main.java
+++ main.java
@@ -1,4 +1,7 @@
 public static void main(String[] args) {
 	String connerie="blague";	
-	System.out.printlf("I NEED COFFEEEE");	
-}
+	System.out.printlf("I NEED COFFEEEE");
+	System.out.println("I need it to !!");
+	System.out.println("I am so tired !!");
+	System.out.println("blah blah");
+	}
