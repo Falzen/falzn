63fa1bd6ccc6c9defaa23a8d9e4a9753a7ecc59a
mounir <mtaarabti@humanbooster.com>
2016-09-01 11:56:13 +0200

    fatigue

--- main.java
+++ main.java
@@ -1,4 +1,5 @@
 public static void main(String[] args) {
 	System.out.printlf("I NEED COFFEEEE");
-	System.out.println("I need it to !!")	
+	System.out.println("I need it to !!");
+	System.out.println("I am so tired !!");	
 }
